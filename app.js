a0c00f3a7fd96b52ca3cb2826228957c1739bac8
fatmacann <canf261012@gmail.com>
2023-03-18 22:08:56 +0300

    partials klasörü

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ import express from"express";
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('index sayfası');
-});
+//ejs template engine
+app.set('view engine', 'ejs');
 
+//static files middleware
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(port, ()=> {
     console.log(`listening on port:${port}`);
