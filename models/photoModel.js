484ad344cb203c2e8c1f30c99cfac430e7612e39
fatmacann <canf261012@gmail.com>
2023-08-13 15:56:30 +0300

    Dashboard page

--- models/photoModel.js
+++ models/photoModel.js
@@ -1,25 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const { Schema} = mongoose
+const { Schema } = mongoose;
 
-const photoSchema =new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    uploadedAt: {
-        type: Date,
-        default: Date.now,
-    },
+const photoSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
-const Photo = mongoose.model('Photo', photoSchema)
+const Photo = mongoose.model('Photo', photoSchema);
 
 export default Photo;
-
